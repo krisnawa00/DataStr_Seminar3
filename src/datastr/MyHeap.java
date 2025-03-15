654e539281ec4220030279a34488ec7ff7dc09e6
EmreOren <m.emreoren@gmail.com>
2025-03-15 14:06:06 +0200

    yay

---
+++ src/datastr/MyHeap.java
@@ -0,0 +1,88 @@
+package datastr;
+
+public class MyHeap<Ttype> {
+	//mainīgie
+	private Ttype[] heap;
+	private final int DEFAULT_SIZE = 10;
+	private int size = DEFAULT_SIZE;
+	private int counter = 0;
+	
+	//bezargumenta konstruktors
+		public MyHeap() {
+			heap = (Ttype[])new Object[size];
+		}
+		
+		//argumenta konstruktors
+		public MyHeap(int inputSize) {
+			if(inputSize > 0) {
+				size = inputSize;
+			}
+			
+			heap = (Ttype[])new Object[size];	
+		}
+		
+		public boolean isFull()
+		{
+			return (counter == size);
+		}
+		
+		public boolean isEmpty() {
+			return (counter == 0);
+		}
+		
+		public int howManyElements() {
+			return counter;
+		}
+		
+		private void resize() {
+			size = (counter < 100) ? size * 2 : (int)(size * 1.5);
+			Ttype[] listNew = (Ttype[]) new Object[size];
+			
+			for(int i = 0; i < counter; i++) {
+				listNew[i] = heap[i];
+			}
+			heap = listNew;
+			System.gc();//izsaucam atkritumu savācēju, kas izdzēsīs mazā masīva elementus
+			
+		}
+		
+		public void enqueue (Ttype element) {
+			if(isFull()) {
+				resize();
+			}
+			
+			heap[counter] = element;
+			counter++;
+			
+			reheapUp(counter -1);
+			
+		}
+		
+		private void reheapUp(int currentChildIndex) {
+			//kreisa berna index = vecaka index * 2 + 1
+			//laba berna index = vecaka index * 2 + 2
+			
+			//vecaka index = (kreisa berna index - 1)/2
+			//vecaka index = (laba berna index - 2)/2
+			
+			// ja berna index ir 6, tad veina gadijuma 2.5 un otra 2
+			
+			int currentParentIndex = (int)((currentChildIndex - 1)/2);
+			
+			if(currentParentIndex >= 0) {
+			if(  ((Comparable)heap[currentParentIndex]).compareTo(heap[currentChildIndex]) == -1){
+				swap(currentParentIndex,currentChildIndex);
+				reheapUp(currentParentIndex);
+			}
+		}
+			}
+
+		private void swap(int index1, int index2) {
+			Ttype temp = heap[index1];
+			heap[index1] = heap[index2];
+			heap[index2] = temp;	
+		}
+		
+		
+
+}
--- src/service/MainService.java
+++ src/service/MainService.java
@@ -3,8 +3,23 @@ package service;
 public class MainService {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//recursionExample1(); <- ja vēlas bezgalīgo rekursiju
+		recursionExample2(10); //10, 9, 8, 6, 5 (pie 5 rekursiju beigsies)
 
+	}
+	
+	//bezgalīgas rekursijas piemērs
+	public static void recursionExample1() {
+		System.out.println("A");
+		recursionExample1();
+	}
+	
+	public static void recursionExample2(int N) {
+		System.out.println("A -> " + N);
+		if(N > 5)
+		{
+			recursionExample2(N-1);
+		}
 	}
 
 }
